4c9b1b7ad0034fdd9d1aad27b37dfda120b65915
Albert Zhang <albert.zhang@infor.com>
2025-06-04 17:41:03 +0800

    PATAL-1003 Split Emergency_Main to Emergency and header Files.

--- PAT/1003_Emergency/1003_Emergency.h
+++ PAT/1003_Emergency/1003_Emergency.h
@@ -14,6 +14,18 @@ typedef struct Road
     struct Road* Next;
 } Road;
 
+typedef struct City
+{
+    int rescueTeamNum;
+    Road* roads;
+} City;
 
+int dataInit(int totalCities, int currCity);
+
+int addRoad(int fromCity, int targetCity, int pathLength);
+
+int shortestPath_Dijkstra(int startCity, int endCity, int totalCities);
+
+int PATAL1003_Emergency();
 
 #endif //INC_1003_EMERGENCY_H
